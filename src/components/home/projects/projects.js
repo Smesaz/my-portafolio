8acdf4b4ede1a4dca91e8cd1fddf1e66ccf80775
Smesaz <semesile@gmail.com>
2022-01-25 21:05:26 -0500

    fixing video screeens 2

--- src/components/home/projects/projects.js
+++ src/components/home/projects/projects.js
@@ -88,7 +88,7 @@ const Projects = () => {
         </div>
       <div class="projects__row">
           <div class="projects__row-img-cont">
-            <iframe title='Pipeta Automática' src="https://player.vimeo.com/video/669013710?h=d1033e49f3"  className='projects__row-video' width={'90%'} height={'100%'}   frameborder="0" allow="autoplay; fullscreen;" allowfullscreen />
+            <iframe title='Pipeta Automática' src="https://player.vimeo.com/video/669013710?h=d1033e49f3"  className='projects__row-video' width={'1100%'} height={'100%'}   frameborder="0" allow="autoplay; fullscreen;" allowfullscreen />
           </div>
           <div class="projects__row-content">
             <h3 class="projects__row-content-title">Pipeta Automática</h3>
@@ -101,7 +101,7 @@ const Projects = () => {
         </div>
       <div class="projects__row">
           <div class="projects__row-img-cont">
-            <iframe title='Dog Breeds App' src="https://player.vimeo.com/video/669019129?h=6dff136ea4"  className='projects__row-video' width={'90%'} height={'100%'}   frameborder="0" allow="autoplay; fullscreen;" allowfullscreen />
+            <iframe title='Dog Breeds App' src="https://player.vimeo.com/video/669019129?h=6dff136ea4"  className='projects__row-video' width={'110%'} height={'100%'}   frameborder="0" allow="autoplay; fullscreen;" allowfullscreen />
           </div>
           <div class="projects__row-content">
             <h3 class="projects__row-content-title">Brazo Robótico Didáctico</h3>
